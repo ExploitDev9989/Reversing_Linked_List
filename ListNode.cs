3f6394beb16574ecf82fcc4130e98676bfc12704
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-03-24 16:37:14 -0400

    Add project files.

---
+++ ListNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversing_Linked_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int value)
+        {
+            val = value;
+            next = null;
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Reversing_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+            ListNode current = head;
+            ListNode prev = null;
+            ListNode temp = head;
+            Console.WriteLine("Original: ");
+            while (temp != null)
+            {
+                Console.Write(temp.val + " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n\n");
+
+            while (current != null)
+            {
+                ListNode next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+
+            }
+            head = prev;
+            temp = head;
+            Console.WriteLine("Reversed: ");
+            while (temp != null)
+            {
+                Console.Write(temp.val+ " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n\n");
+
+          
+
+        }
+    }
+}
